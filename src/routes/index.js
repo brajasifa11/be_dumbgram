49ddce0353894cd4148f8b7971912755ddf3bbc1
brajasifa11 <brajasifa11@gamil.com>
2021-06-15 17:37:11 +0700

    remove comment

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
-// const { getData, getDetail, addData, updateData, deleteData } = require('../controllers/dataUsers')
 const { users, register } = require('../controllers/user')
-
-//todo
-// router.get('/', getData)
-// router.get('/detail/:id', getDetail)
-// router.post('/data', addData)
-// router.patch('/data/:id', updateData)
-// router.delete('/data/:id', deleteData)
 
 //register
 router.get('/register', register)
